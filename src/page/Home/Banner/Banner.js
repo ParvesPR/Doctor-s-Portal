94c2edb5c140d331951f40934457c89b636d2a0a
ParvesPR <parves2585@gmail.com>
2022-05-10 21:51:05 +0600

    banner section added

---
+++ src/page/Home/Banner/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import chair from '../../../assets/images/chair.png';
+
+const Banner = () => {
+    return (
+        <div class="hero min-h-screen">
+            <div class="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} class="max-w-lg rounded-lg shadow-2xl mb-4" />
+                <div>
+                    <h1 class="text-5xl font-bold mt-5">Your New Smile Starts Here</h1>
+                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button class="btn btn-#19D3AE">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
