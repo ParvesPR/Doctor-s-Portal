416fbdf283d08ca87cd91d6700b4167386428f82
ParvesPR <parves2585@gmail.com>
2022-05-11 21:28:22 +0600

    service component added

--- src/page/Home/Info/Info.js
+++ src/page/Home/Info/Info.js
@@ -6,7 +6,7 @@ import InfoCard from '../InfoCard/InfoCard';
 
 const Info = () => {
     return (
-        <div className='px-5 grid grid-cols-1 lg:grid-cols-3 gap-5'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
             <InfoCard cardTitle={'Opening Hours'} bgClass={'bg-gradient-to-r from-secondary to-primary'} img={clock}></InfoCard>
             <InfoCard cardTitle={'Opening Hours'} bgClass={'bg-accent'} img={marker}></InfoCard>
             <InfoCard cardTitle={'Opening Hours'} bgClass={'bg-gradient-to-r from-secondary to-primary'} img={phone}></InfoCard>
---
+++ src/page/Home/Services/Services.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import fluoride from '../../../assets/images/fluoride.png';
+import cavity from '../../../assets/images/cavity.png';
+import whitening from '../../../assets/images/whitening.png'
+import Service from './Service/Service';
+
+const Services = () => {
+    const services = [
+        {
+            _id: 1,
+            name: 'Fluoride Treatment',
+            description: '',
+            img: 'fluoride'
+        },
+        {
+            _id: 2,
+            name: 'Cavity Filling',
+            description: '',
+            img: 'cavity'
+        },
+        {
+            _id: 1,
+            name: 'Teeth Whitening',
+            description: '',
+            img: 'whitening'
+        },
+    ]
+    return (
+        <div>
+            <div className='text-center py-20'>
+                <h3 className='text-secondary mb-2 font-semibold text-2xl'>OUR SERVICES</h3>
+                <h2 className='text-4xl'>Services We Provide</h2>
+            </div>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    services.map(service =>
+                        <Service
+                            key={service._id}
+                            service={service}
+                        ></Service>
+                    )
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
