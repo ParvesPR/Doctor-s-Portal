f08958863cf4342e0396c96822f1f7b533026986
ParvesPR <parves2585@gmail.com>
2022-05-12 12:20:00 +0600

    review component added

--- src/page/Home/Testimonials/Testimonial.js
+++ src/page/Home/Testimonials/Testimonial.js
@@ -3,40 +3,48 @@ import people1 from '../../../assets/images/people1.png';
 import people2 from '../../../assets/images/people2.png';
 import people3 from '../../../assets/images/people3.png';
 import quote from '../../../assets/icons/quote.svg';
+import Review from './Review/Review';
 const Testimonial = () => {
     const reviews = [
         {
             _id: 1,
             name: 'Winson Herry',
             location: 'New York',
-            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribute to using Content here, content',
             img: people1
         },
         {
             _id: 2,
             name: 'Winson Herry',
             location: 'New York',
-            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribute to using Content here, content',
             img: people2
         },
         {
             _id: 3,
             name: 'Winson Herry',
             location: 'New York',
-            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribute to using Content here, content',
             img: people3
         },
     ]
     return (
         <section className='my-24'>
             <div className='flex justify-between'>
-                <div>
+                <div className='mb-5'>
                     <h3 className='text-primary font-bold text-xl'>Testimonial</h3>
                     <h4 className='text-accent text-2xl'>What Our Patients Says</h4>
                 </div>
                 <div><img src={quote} className="w-16 lg:w-48" alt="" /></div>
             </div>
-            <div></div>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    reviews.map(review => <Review
+                        key={review._id}
+                        review={review}
+                    ></Review>)
+                }
+            </div>
         </section>
     );
 };
