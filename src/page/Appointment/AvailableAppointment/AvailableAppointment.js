73968bc4d6dfcb7619be4f7ddeb293ae94fd6750
ParvesPR <parves2585@gmail.com>
2022-05-13 20:47:14 +0800

    dynamically show date on modal

--- src/page/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/page/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -24,7 +24,7 @@ const AvailableAppointment = ({ date }) => {
                     ></AppointmentService>)
                 }
             </div>
-            {treatment && <BookingModal treatment={treatment}></BookingModal>}
+            {treatment && <BookingModal date={date} treatment={treatment}></BookingModal>}
         </div >
     );
 };
--- src/page/Appointment/BookingModal/BookingModal.js
+++ src/page/Appointment/BookingModal/BookingModal.js
@@ -1,6 +1,7 @@
+import { format } from 'date-fns';
 import React from 'react';
 
-const BookingModal = ({ treatment }) => {
+const BookingModal = ({ treatment, date }) => {
     const { name, slots } = treatment;
     return (
         <div>
@@ -9,10 +10,14 @@ const BookingModal = ({ treatment }) => {
                 <div className="modal-box">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2 bg-gradient-to-r from-secondary to-primary text-white border-none">✕</label>
                     <h3 className="font-bold text-lg">Booking for: <span className='text-secondary font-bold'>{name}</span></h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                    <div className="modal-action">
-                        <label htmlFor="booking-modal" className="btn bg-gradient-to-r from-secondary to-primary text-white border-none">Yay!</label>
-                    </div>
+                    <form className='grid grid-cols-1 gap-3 justify-items-center'>
+                        <input type="text" value={format(date, 'PP')} className="mt-5 input input-bordered w-full max-w-xs" readOnly/>
+                        <input type="text" placeholder="Name" className="input input-bordered w-full max-w-xs" />
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                        <input type="submit" value="Submit" className="btn bg-gradient-to-r from-secondary to-primary text-white border-none w-full max-w-xs" />
+                    </form>
                 </div>
             </div>
         </div>
