925ca8b97f4847432e49b0a82eee436ed10663c5
ParvesPR <parves2585@gmail.com>
2022-05-13 20:56:47 +0800

    dynamically show time slot for appointment

--- src/page/Appointment/BookingModal/BookingModal.js
+++ src/page/Appointment/BookingModal/BookingModal.js
@@ -11,11 +11,15 @@ const BookingModal = ({ treatment, date }) => {
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2 bg-gradient-to-r from-secondary to-primary text-white border-none">✕</label>
                     <h3 className="font-bold text-lg">Booking for: <span className='text-secondary font-bold'>{name}</span></h3>
                     <form className='grid grid-cols-1 gap-3 justify-items-center'>
-                        <input type="text" value={format(date, 'PP')} className="mt-5 input input-bordered w-full max-w-xs" readOnly/>
-                        <input type="text" placeholder="Name" className="input input-bordered w-full max-w-xs" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                        <input type="text" value={format(date, 'PP')} className="mt-5 input input-bordered w-full max-w-xs" readOnly />
+                        <select class="select select-bordered w-full max-w-xs">
+                            {
+                                slots.map(slot => <option value={slot}>{slot}</option>)
+                            }
+                        </select>
+                        <input type="text" name="name" placeholder="Your Name" className="input input-bordered w-full max-w-xs" required />
+                        <input type="email" name="email" placeholder="Email Address" className="input input-bordered w-full max-w-xs" required />
+                        <input type="number" name="phone" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" required />
                         <input type="submit" value="Submit" className="btn bg-gradient-to-r from-secondary to-primary text-white border-none w-full max-w-xs" />
                     </form>
                 </div>
