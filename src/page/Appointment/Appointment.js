aa8cb58f9a3769e5115842d266e01c5a0455a109
ParvesPR <parves2585@gmail.com>
2022-05-13 09:49:38 +0600

    appoinment service component added

--- src/page/Appointment/Appointment.js
+++ src/page/Appointment/Appointment.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import Footer from '../Shared/Footer/Footer';
 import AppointmentBanner from './AppointmentBanner/AppointmentBanner';
+import AppointmentService from './AppointmentService/AppointmentService';
 import AvailableAppointment from './AvailableAppointment/AvailableAppointment';
+
+
 const Appointment = () => {
     const [date, setDate] = useState(new Date())
     return (
         <div>
             <AppointmentBanner date={date} setDate={setDate}></AppointmentBanner>
             <AvailableAppointment date={date}></AvailableAppointment>
+            <AppointmentService></AppointmentService>
             <Footer></Footer>
         </div>
     );
--- src/page/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/page/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,10 +1,26 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AppointmentService from '../AppointmentService/AppointmentService';
 
-const AvailableAppointment = ({date}) => {
+const AvailableAppointment = ({ date }) => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
     return (
         <div>
-            <p className='text-center text-secondary font-semibold text-xl'>Available Appointment on {format(date, 'PP')}</p>
+            <h4 className='text-center text-secondary font-semibold text-xl'>Available Appointment on {format(date, 'PP')}</h4>
+            <div>
+                {
+                    services.map(service => <AppointmentService
+                        key={service._id}
+                        service={service}
+                    ></AppointmentService>)
+                }
+            </div>
         </div>
     );
 };
