d4e11f3c88b854a8da93ef76160d2efda5f15c4c
ParvesPR <parves2585@gmail.com>
2022-05-14 11:11:45 +0800

    implement google sign in

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,18 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    // apiKey: process.env.REACT_APP_API_KEY,
+    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    // projectId: process.env.REACT_APP_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    // appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyCxSInwUfiORsGMGxKWcV_-dsJEjtF53Ak",
+    authDomain: "doctors-portal-bf66e.firebaseapp.com",
+    projectId: "doctors-portal-bf66e",
+    storageBucket: "doctors-portal-bf66e.appspot.com",
+    messagingSenderId: "1050579346663",
+    appId: "1:1050579346663:web:49c7e0a203ce59be436e42"
 };
 
 // Initialize Firebase
--- src/page/Login/Login.js
+++ src/page/Login/Login.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     return (
         <div className='flex h-screen justify-center items-center'>
             <div className="card lg:max-w-lg shadow-xl">
                 <div className="card-body">
                     <h2 className="text-center font-bold text-2xl">Login</h2>
                     <button className='btn btn-secondary text-white'>Login</button>
-                    <div class="divider">OR</div>
-                    <button class="btn btn-outline">Continue with Google</button>
+                    <div className="divider">OR</div>
+                    <button className="btn btn-outline"
+                        onClick={() => signInWithGoogle()}
+                    >Continue with Google</button>
                 </div>
             </div>
         </div>
