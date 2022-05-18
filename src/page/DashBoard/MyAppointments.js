80e379cff2240a8bc448626e1ea8351c9dd636d3
ParvesPR <parves2585@gmail.com>
2022-05-18 14:28:53 +0800

    verify token and get access

--- src/page/DashBoard/MyAppointments.js
+++ src/page/DashBoard/MyAppointments.js
@@ -1,20 +1,31 @@
+import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const MyAppointments = () => {
     const [appointments, setAppointments] = useState([]);
     const [user] = useAuthState(auth);
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (user) {
-            fetch(`http://localhost:5000/appointments?patient=${user.email}`,{
-                method:'GET',
-                headers:{
-                    'authorization':`Bearer ${localStorage.getItem('accessToken')}`
+            fetch(`http://localhost:5000/appointments?patient=${user.email}`, {
+                method: 'GET',
+                headers: {
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
                 }
             })
-                .then(res => res.json())
+                .then(res => {
+                    console.log('res', res);
+                    if (res.status === 401 || res.status === 403) {
+                        signOut(auth);
+                        localStorage.removeItem('accessToken');
+                        navigate('/')
+                    }
+                    return res.json()
+                })
                 .then(data => setAppointments(data));
         }
     }, [user])
@@ -29,13 +40,13 @@ const MyAppointments = () => {
                             <th>Index</th>
                             <th>Name</th>
                             <th>Date</th>
-                            <th>Time</th> 
-                            <th>Treatment</th> 
+                            <th>Time</th>
+                            <th>Treatment</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            appointments.map((a,index) => <tr>
+                            appointments.map((a, index) => <tr>
                                 <td className='font-bold'>{index + 1}</td>
                                 <td>{a.patientName}</td>
                                 <td>{a.date}</td>
